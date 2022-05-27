2c0282d59584c2062cf5085bc92d485639cfb71c
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-05-26 19:48:27 -0500

    app.js changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
