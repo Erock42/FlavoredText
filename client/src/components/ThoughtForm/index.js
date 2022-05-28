e1a975f9cecb74e3211deb0510e0a0801a9b85e7
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-28 13:04:45 -0500

    added palette

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -6,7 +6,7 @@ import { ADD_THOUGHT } from '../../utils/mutations';
 import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 import Auth from '../../utils/auth';
-
+import '../../app.css';
 const ThoughtForm = () => {
   const [thoughtText, setThoughtText] = useState('');
 
