8a8602dbe08274e78077bb15cc90981293eb9806
Jeremiah <jclanc7507@gmail.com>
2022-05-29 18:44:50 -0400

    downloaded Sadie's code, branched off

---
+++ client/src/components/Recipe/Recipe.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Recipe.css";
+
+const Recipe = ({ title, image, ingredients }) => {
+  return (
+    <div className="recipe">
+      <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
+      <img className="style.image" src={image} alt="" />
+
+      {/* need to change/add full recipe page (maybe) */}
+      <button className='search-button'><a href="https://www.edamam.com/recipes/" target="_blank">Full recipe</a></button>
+      
+    </div>
+  );
+};
+
+export default Recipe;
