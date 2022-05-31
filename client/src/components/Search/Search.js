391760022832de81af7660ddfb38a45a014ef682
Diego Kaku <diegokaku@gmail.com>
2022-05-31 18:56:49 -0500

    working version, top 5 recipe

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -5,17 +5,17 @@ import './Search.css';
 
 // test random recipe
 // display random recipe
-// const topRecipes = [
-//   {
-//     1: { URL: 'https://www.edamam.com/results/recipe/?recipe=campfire-feijoada-4b04e5290e4196f7d3e8692684b90957', label: 'Campfire Feijoada' },
-//     2: { URL: 'https://www.edamam.com/results/recipe/?recipe=pasta-alla-gricia-recipe-20022d91be0968092a8eab1aceee81be', label: 'Pasta alla Gricia' },
-//     3: { URL: 'https://www.edamam.com/results/recipe/?recipe=pizza-margherita-recipe-9b9b8b8b9b8b9b8b9b8b9b8b9b8b9b8b', label: 'Pizza Margherita' },
-//     4: { URL: 'https://www.edamam.com/results/recipe/?recipe=chicken-teriyaki-888e9fc4a808e9e4ccdb2ac24a6a2f46', label: 'Chicken Teriyaki' },
-//     5: { URL: 'https://www.edamam.com/results/recipe/?recipe=ikura-(salmon-caviar)-5cfd32ab67396a6249f599b2f53e6b57', label: 'Ikura (Salmon Caviar)' },
-//   }];
+const topRecipes = [
+  {
+    1: { URL: 'https://www.edamam.com/results/recipe/?recipe=campfire-feijoada-4b04e5290e4196f7d3e8692684b90957', label: 'Campfire Feijoada' },
+    2: { URL: 'https://www.edamam.com/results/recipe/?recipe=pasta-alla-gricia-recipe-20022d91be0968092a8eab1aceee81be', label: 'Pasta alla Gricia' },
+    3: { URL: 'https://www.edamam.com/results/recipe/?recipe=pizza-margherita-recipe-9b9b8b8b9b8b9b8b9b8b9b8b9b8b9b8b', label: 'Pizza Margherita' },
+    4: { URL: 'https://www.edamam.com/results/recipe/?recipe=chicken-teriyaki-888e9fc4a808e9e4ccdb2ac24a6a2f46', label: 'Chicken Teriyaki' },
+    5: { URL: 'https://www.edamam.com/results/recipe/?recipe=ikura-(salmon-caviar)-5cfd32ab67396a6249f599b2f53e6b57', label: 'Ikura (Salmon Caviar)' },
+  }];
 
-// const randomRecipe = topRecipes[Math.floor(Math.random() * topRecipes.length)];
-// console.log(randomRecipe);
+const randomRecipe = topRecipes[Math.floor(Math.random() * topRecipes.length)];
+console.log(randomRecipe);
 
 const Search = () => {
   const [recipes, setRecipes] = useState([]);
@@ -57,13 +57,13 @@ const Search = () => {
       </div>
 
       {/* very basic "random recipes" */}
-      {/* <div className='recipes' >
-        <button className='search-button' href={randomRecipe[1].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[1].label}</button>
-        <button className='search-button' href={randomRecipe[2].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[2].label}</button>
-        <button className='search-button' href={randomRecipe[3].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[3].label}</button>
-        <button className='search-button' href={randomRecipe[4].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[4].label}</button>
-        <button className='search-button' href={randomRecipe[5].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[5].label}</button>
-      </div> */}
+      <div className='recipes' >
+        <a className='search-button' href={randomRecipe[1].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[1].label}</a>
+        <a className='search-button' href={randomRecipe[2].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[2].label}</a>
+        <a className='search-button' href={randomRecipe[3].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[3].label}</a>
+        <a className='search-button' href={randomRecipe[4].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[4].label}</a>
+        <a className='search-button' href={randomRecipe[5].URL} target="_blank" rel="noopener noreferrer">{randomRecipe[5].label}</a>
+      </div>
     </div >
   );
 };
