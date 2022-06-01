ced1a31913b6b42984c4a8a238b9769eb379bbcd
Jeremiah <jclanc7507@gmail.com>
2022-06-01 18:49:54 -0400

    working on updating thought, partly done

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -7,6 +7,7 @@ import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 import Auth from '../../utils/auth';
 import '../../app.css';
+
 const ThoughtForm = () => {
   const [thoughtText, setThoughtText] = useState('');
 
@@ -91,7 +92,7 @@ const ThoughtForm = () => {
 
             <div className="col-12 col-lg-3">
               <button className="btn btn-primary btn-block py-3" type="submit">
-                Add Recipe
+                Add Thought
               </button>
             </div>
             {error && (
--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { DELETE_THOUGHT } from '../../utils/mutations';
-import { QUERY_ME } from '../../utils/queries';
+import { UPDATE_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
 import Auth from '../../utils/auth';
 
 const ThoughtList = ({
@@ -12,7 +14,12 @@ const ThoughtList = ({
   showUsername = true,
 }) => {
 
-  const [deleteThought, { error }] = useMutation(DELETE_THOUGHT,
+  const [thoughtText, setThoughtText] = useState('');
+
+  const [characterCount, setCharacterCount] = useState(0);
+
+// delete thought
+  const [deleteThought] = useMutation(DELETE_THOUGHT,
     {refetchQueries: [ QUERY_ME ]
     })
 
@@ -31,6 +38,75 @@ const ThoughtList = ({
     }
     catch (err) {
       console.error(err);
+    }
+  };
+  // update thought
+  const [updateThought, { error }] = useMutation(UPDATE_THOUGHT, {
+    update(cache, { data: { updateThought } }) {
+      try {
+        const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+        cache.writeQuery({
+          query: QUERY_THOUGHTS,
+          data: { thoughts: [updateThought, ...thoughts] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      // update me object's cache
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, thoughts: [...me.thoughts, updateThought] } },
+      });
+    },
+  });
+    
+    const handleUpdateThought = async (event) => {
+      event.preventDefault();
+
+    try {
+      const { data } = await updateThought({
+        variables: {
+          thoughtText,
+          thoughtAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      console.log(data);
+
+      setThoughtText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await updateThought({
+        variables: {
+          thoughtText,
+          thoughtAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      console.log(data);
+
+      setThoughtText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'thoughtText' && value.length <= 280) {
+      setThoughtText(value);
+      setCharacterCount(value.length);
     }
   };
 
@@ -72,14 +148,41 @@ const ThoughtList = ({
             >
               Join the discussion on this thought.
             </Link>
-            <div
-              
+            <p
+            className={`m-0 ${characterCount === 280 || error ? 'text-danger' : ''
+              }`}
+            >
+              Character Count: {characterCount}/280
+            </p>
+            <form>
+              <div              
+                id={thought._id}
+                onSubmit={handleFormSubmit}
+                className="btn --secondaryAccent btn-block btn-squared"              
+              >
+                <textarea
+                  name="thoughtText"
+                  placeholder="What's changed?"
+                  value={thoughtText}
+                  className="form-input w-100"
+                  style={{ lineHeight: '1.5', resize: 'vertical' }}
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+              <button className="btn btn-primary btn-block btn-squared" type="submit">                
+                Edit thought 🗨️
+              </button>
+              {error && (
+                <div className="col-12 my-3 bg-danger text-white p-3">
+                  {error.message}
+                </div>
+              )}
+            </form>
+            <div              
               id={thought._id}
               onClick={handleDeleteThought}
-              className="btn btn-danger btn-block btn-squared"
-              
-            >
-              Delete this thought X
+              className="btn btn-danger btn-block btn-squared cursor">              
+              Delete this thought 🗑️
             </div>
           </div>
         ))}
