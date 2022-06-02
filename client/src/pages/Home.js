d2fa4032ee2a0507a14663d60412d274f60c4def
Erock FaQue <eric.kemerer@gmail.com>
2022-06-02 18:34:36 -0400

    centered comments/thoughts, commented out small details

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,15 +18,15 @@ const Home = () => {
       <div className="container">
       <div className='flex-row container'>
         <div className='col-12 col mb-3 p-1' style={{ border: '1px dotted #1a1a1a' }}>
-          ***** this one needs to go to the left side *****
+          {/****** this one needs to go to the left side ******/}
           <Search />
         </div>
         <div className='col-12 col-6 mb-3 p-2' style={{ border: '1px dotted #1a1a1a' }}>
-          ***** this one needs to go to the right side *****
+            {/***** this one needs to go to the right side *****/}
           {/*<TopRecipes />*/}
         </div>
         <div className='col-12 col mb-3 p-3' style={{ border: '1px dotted #1a1a1a' }}>
-          ***** stay on the middle *****
+            {/***** stay on the middle *****/}
           <ThoughtForm />
         </div>
         <div className='col-12 col-md-8 mb-3'>{loading ? <div>Loading...</div> : <ThoughtList thoughts={thoughts} title='Some Feed for Thought(s)...' />}</div>
